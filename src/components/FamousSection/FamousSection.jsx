4d9a5bf3f1ee3d64ee636ce425fa681df0c6e492
Thomas-Vargas <tavargas2012irc@gmail.com>
2023-03-21 15:22:43 -0500

    Finished base goals.

--- src/components/FamousSection/FamousSection.jsx
+++ src/components/FamousSection/FamousSection.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './FamousSection.css';
 
 function FamousSection() {
@@ -7,9 +8,20 @@ function FamousSection() {
   let [famousPeopleArray, setPeopleArray] = useState([]);
 
   // TODO: on load, call the fetchPeople() function
+  useEffect(() => {
+    fetchPeople();
+  }, []);
 
   const fetchPeople = () => {
     // TODO: fetch the list of people from the server
+    axios.get('/people')
+      .then(response => {
+        console.log(response.data);
+        setPeopleArray(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      })
   }
 
   const addPerson = (evt) => {
@@ -20,23 +32,36 @@ function FamousSection() {
 
     // HINT: the server is expecting a person object 
     //       with a `name` and a `role` property
-  
+
+    axios.post('/people', {name: famousPersonName, role: famousPersonRole})
+      .then(response => {
+        console.log(response);
+        fetchPeople();
+      })
+      .catch(error => {
+        console.log(error);
+      })
+
+    setPersonName('');
+    setPersonRole('');
   }
 
     return (
       <section className="new-person-section">
         <form onSubmit={addPerson}>
           <label htmlFor="name-input">Name:</label>
-          <input id="name-input" onChange={e => setPersonName(e.target.value)} />
+          <input id="name-input" value={famousPersonName} onChange={e => setPersonName(e.target.value)} />
           <label htmlFor="role-input">Famous for:</label>
-          <input id="role-input" onChange={e => setPersonRole(e.target.value)} />
+          <input id="role-input" value={famousPersonRole} onChange={e => setPersonRole(e.target.value)} />
           <button type="submit">Done</button>
         </form>
-        <p>
-          {famousPersonName} is famous for "{famousPersonRole}".
-        </p>
         <ul>
           {/* TODO: Render the list of famous people */}
+          {famousPeopleArray.map(person => {
+            return (
+              <p key={person.id}>{person.name} is famous for "{person.role}"</p>
+            )
+          })}
         </ul>
       </section>
     );
